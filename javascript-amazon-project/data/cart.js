9b4f5e8cb6775f9f5c4f2bccb99dc15c493b980e
Jhidro_Dev <jbyiringirod@gmail.com>
2025-02-09 15:49:04 +0200

    Generate HTML for the order summary

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,6 +1,13 @@
 export const cart = [
-
-];
+    {
+        productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+        quantity: 2
+    },
+    {
+        productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+        quantity: 7
+    }
+    ];
 export function addToCart (productId) {
         let matchingIterm;
         cart.forEach((cartItem) => {
