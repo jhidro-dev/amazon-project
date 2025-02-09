1ca2feb6df0540d575750ac3b599624a4d27ffd2
Jhidro_Dev <jbyiringirod@gmail.com>
2025-02-09 17:04:55 +0200

    Fix price formatting and radio selection

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,11 +1,15 @@
 export const cart = [
     {
         productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
-        quantity: 2
+        quantity: 1
     },
     {
         productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-        quantity: 7
+        quantity: 1
+    },
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
     }
     ];
 export function addToCart (productId) {
