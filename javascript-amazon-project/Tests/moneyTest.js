ab6cdc1bdee8f6a8171f31492bcf42b9e7dc9361
Jhidro_Dev <jbyiringirod@gmail.com>
2025-02-13 15:53:29 +0200

    Add jasmine testing framework

--- javascript-amazon-project/Tests/moneyTest.js
+++ javascript-amazon-project/Tests/moneyTest.js
@@ -1,5 +1,5 @@
 import {formatCurrency} from "../scripts/utils/money.js";
-if (formatCurrency(2000.4) === '20.00') {
+if (formatCurrency(2000.4) === '20.00' ) {
     console.log('passed');
 } else{
     console.log('feiled');
